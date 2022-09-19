87e3187cabdb77f5c9a1e302c8c80d34b6ae360f
arinde <arindevictor8@gmail.com>
2022-09-19 16:36:17 -0700

    my third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: int lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (s[length])
+		lenght++;
+	return (lenght);
+}
