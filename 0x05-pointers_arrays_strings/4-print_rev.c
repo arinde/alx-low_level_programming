c337d123052261c40c19a9a2c1ca3b04010a988f
arinde <arindevictor8@gmail.com>
2022-09-19 17:16:40 -0700

    fifth_commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,19 +9,11 @@
 
 void print_rev(char *s)
 {
-	int l, i;
-	char ch;
+	int i = 0;
 
-	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
-		;
-
-	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)
-	{
-		ch = s[i];
-		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-		s[l - 1 - i] = ch;
-	}
-
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
